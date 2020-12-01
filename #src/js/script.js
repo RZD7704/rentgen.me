4f49010724306b2b24793b1348229e8f5c4dda51
Artem <artemius2014poltava@gmail.com>
2020-12-01 13:43:33 +0200

    Add editing of profile

--- #src/js/script.js
+++ #src/js/script.js
@@ -65,22 +65,136 @@ window.onload = function() {
         });
     }
 
-    function loadCharts() {
+    function addEditing() {
+        const editingBtn = document.querySelector('.editing-btn-js');
+        let flag = true;
+        // let inputVal;
+
+        editingBtn.addEventListener('click', function(e) {
+            e.preventDefault();
+            creatingEditingFields();
+        });
+        function creatingEditingFields() {
+            const fieldsBlock = document.querySelector('.profile-mask__list-editing'),
+                //   parentFields = document.querySelector('.profile-mask__list'),
+                  archivBlock = document.querySelector('.profile-mask__list-edited'),
+                  parentPhoto = document.querySelector('.profile-mask__photo');
+
+            let photoEditing;
+
+            if(!flag) return;
+            archivBlock.innerHTML = "";
+            fieldsBlock.innerHTML = `
+                <div class="profile-mask__list-wgt">
+                    <div class="profile-mask__list-headline">Стаж роботи:</div>
+                    <input class="profile-mask__list-field" id="input-edit" type="text" placeholder="Введіть дані">
+                </div>
+                <div class="profile-mask__list-wgt">
+                    <div class="profile-mask__list-headline">Місто:</div>
+                    <input class="profile-mask__list-field" type="text" placeholder="Введіть дані">
+                </div>
+                <div class="profile-mask__list-wgt">
+                    <div class="profile-mask__list-headline">Місце роботи:</div>
+                    <input class="profile-mask__list-field" type="text" placeholder="Введіть дані">
+                </div>
+                <div class="profile-mask__list-wgt">
+                    <div class="profile-mask__list-headline">Спеціалізація:</div>
+                    <input class="profile-mask__list-field" type="text" placeholder="Введіть дані">
+                </div>
+                <div class="profile-mask__list-wgt">
+                    <div class="profile-mask__list-headline">Оберіть тип:</div>
+                    <div class="d-flex">
+                        <div class="profile-mask__list-wgt-btn radio-btn-js mr-2">
+                            <input class="profile-mask__list-wgt-btn--input" id="radioKT" type="checkbox" name="radio1" value="1">
+                            <label for="radioKT">КТ</label>
+                        </div>
+                        <div class="profile-mask__list-wgt-btn radio-btn-js">
+                            <input class="profile-mask__list-wgt-btn--input" id="radioMRT" type="checkbox" name="radio2" value="1">
+                            <label for="radioMRT">МРТ</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="profile-mask__list-wgt">
+                    <div class="profile-mask__list-headline">Про лікаря:</div>
+                    <input class="profile-mask__list-field" type="text" placeholder="Введіть дані">
+                </div>
+                <button class="profile-mask__list-editing-btn mx-auto d-block mt-2">Зберегти</button>
+            `;
+            photoEditing = document.createElement('a');
+            photoEditing.classList.add('profile-mask__photo-editing');
+            photoEditing.innerHTML = `<span>+</span>`;
+            parentPhoto.appendChild(photoEditing);
+            flag = false;
+            // inputVal = document.getElementById('elem1').value;
+
+            saveBtn = document.querySelector('.profile-mask__list-editing-btn');
+            saveBtn.addEventListener('click', function(e) {
+                e.preventDefault();
+                creatingEditedFields(photoEditing);
+                // console.log(inputVal);
+            });
+        }
+
+        function creatingEditedFields(photoParent) {
+            const fieldsBlock = document.querySelector('.profile-mask__list-edited'),
+                  parentFields = document.querySelector('.profile-mask__list'),
+                  archivBlock = document.querySelector('.profile-mask__list-editing');
+
+            photoParent.remove();
+            archivBlock.innerHTML = "";
+            fieldsBlock.innerHTML = `
+                <div class="profile-mask__list-wgt">
+                    <div class="profile-mask__list-headline">Стаж роботи:</div>
+                    <div class="profile-mask__list-desc">9 років</div>
+                </div>
+                <div class="profile-mask__list-wgt">
+                    <div class="profile-mask__list-headline">Місто:</div>
+                    <div class="profile-mask__list-desc">Полтава</div>
+                </div>
+                <div class="profile-mask__list-wgt">
+                    <div class="profile-mask__list-headline">Місце роботи:</div>
+                    <div class="profile-mask__list-desc">УМСА</div>
+                </div>
+                <div class="profile-mask__list-wgt">
+                    <div class="profile-mask__list-headline">Спеціалізація:</div>
+                    <div class="profile-mask__list-desc">лікар радіолог, рентгенолог ІІ категорії</div>
+                </div>
+                <div class="profile-mask__list-wgt">
+                    <div class="profile-mask__list-headline">Діагностики:</div>
+                    <div class="profile-mask__list-desc">КТ, МРТ</div>
+                </div>
+                <div class="profile-mask__list-wgt">
+                    <div class="profile-mask__list-headline">Про лікаря:</div>
+                    <div class="profile-mask__list-desc">Опис відсутній</div>
+                </div>
+            `;
+            parentFields.append(fieldsBlock);
+            flag = true;
+        }
+    }
+
+    function checkingBlocks() {
         let doctorChart = document.querySelector('#chart'),
             doctorChartChief = document.querySelector('#chart-chief'),
-            chiefDoughnatChart = document.querySelector('#chief-doughnat-chart');
+            chiefDoughnatChart = document.querySelector('#chief-doughnat-chart'),
+            editBtn = document.querySelector('.editing-btn-js');
         
+        if (editBtn) {
+            addEditing();
+        } 
         if (doctorChart) {
             barChart('#chart', [[67.8], [20.7], [15.4], [11.4]], ['#12D3CE', '#A2EEEC', '#A481DE', '#C6B7DF'], ['КТ', 'МРТ', 'Рентген', 'КЛТ'], ['Risk Level']);  
-        } else if (doctorChartChief) {
+        } 
+        if (doctorChartChief) {
             barChart('#chart-chief', [[67.8], [20.7], [15.4], [11.4]], ['#12D3CE', '#A2EEEC', '#A481DE', '#C6B7DF'], ['КТ', 'МРТ', 'Рентген', 'КЛТ'], ['Risk Level']); 
-        } else if (chiefDoughnatChart) {
-            addPieChart('#chief-doughnat-chart', [5, 3], ['Опрацьовані', 'Неопрацьовані'], ['rgba(162, 238, 236, 1)', 'rgba(218, 205, 242, 1)']); 
         }
+        if (chiefDoughnatChart) {
+            addPieChart('#chief-doughnat-chart', [5, 3], ['Опрацьовані', 'Неопрацьовані'], ['rgba(162, 238, 236, 1)', 'rgba(218, 205, 242, 1)']); 
+        } 
         
     }
     
-    loadCharts();
+    checkingBlocks();
 
     var swiperChief = new Swiper('.chief-profiles__swiper', {
         slidesPerView: 2,
@@ -157,6 +271,5 @@ jQuery( document ).ready(function($) {
             $('[data-stage="' + i + '"]').fadeIn('slow');
         });
     });
-
 });
 
